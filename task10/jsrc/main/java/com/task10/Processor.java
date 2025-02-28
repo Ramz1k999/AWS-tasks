3cc4039d659dbbeb4322eca49a8499a0c1fdc3d0
Jaloladdin Yaxshimurodov <jaloladdin_yaxshimurodov@student.itpu.uz>
2025-02-28 16:16:30 -0500

    all

--- task10/jsrc/main/java/com/task10/Processor.java
+++ task10/jsrc/main/java/com/task10/Processor.java
@@ -1,5 +1,7 @@
 package com.task10;
 
+
+import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
