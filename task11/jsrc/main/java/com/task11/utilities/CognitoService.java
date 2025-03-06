352334041954cba4092796f92f5f4a4a0244f8d5
Ramsey <jaloladdin_yaxshimurodov@epam.com>
2025-03-06 04:14:19 -0500

    all

--- task11/jsrc/main/java/com/task11/utilities/CognitoService.java
+++ task11/jsrc/main/java/com/task11/utilities/CognitoService.java
@@ -24,33 +24,43 @@ public class CognitoService {
     }
 
     public Map<String, Object> signUp(Map<String, Object> request) {
-        AdminCreateUserRequest signUpRequest = AdminCreateUserRequest.builder()
-                .userPoolId(userPoolId)
-                .username((String) request.get("email"))
-                .temporaryPassword((String) request.get("password"))
-                .messageAction("SUPPRESS")
-                .userAttributes(
-                        AttributeType.builder().name("given_name").value((String) request.get("firstName")).build(),
-                        AttributeType.builder().name("family_name").value((String) request.get("lastName")).build(),
-                        AttributeType.builder().name("email").value((String) request.get("email")).build()
-                )
-                .build();
+        try {
+            AdminCreateUserRequest signUpRequest = AdminCreateUserRequest.builder()
+                    .userPoolId(userPoolId)
+                    .username((String) request.get("email"))
+                    .messageAction("SUPPRESS")  // Don't send automatic email
+                    .userAttributes(
+                            AttributeType.builder().name("given_name").value((String) request.get("firstName")).build(),
+                            AttributeType.builder().name("family_name").value((String) request.get("lastName")).build(),
+                            AttributeType.builder().name("email").value((String) request.get("email")).build()
+                    )
+                    .clientMetadata(Map.of()) // Ensure client metadata is set (even if empty)
+                    .build();
 
-        cognitoClient.adminCreateUser(signUpRequest);
-        return Map.of("statusCode", 200, "body", "User created");
+            AdminCreateUserResponse response = cognitoClient.adminCreateUser(signUpRequest);
+            return Map.of("statusCode", 200, "body", "User created: " + response.user().username());
+
+        } catch (CognitoIdentityProviderException e) {
+            return Map.of("statusCode", 500, "error", "Cognito signup error: " + e.awsErrorDetails().errorMessage());
+        }
     }
 
     public Map<String, Object> signIn(Map<String, Object> request) {
-        InitiateAuthRequest authRequest = InitiateAuthRequest.builder()
-                .authFlow(AuthFlowType.USER_PASSWORD_AUTH)
-                .clientId(clientId)
-                .authParameters(Map.of(
-                        "USERNAME", (String) request.get("email"),
-                        "PASSWORD", (String) request.get("password")
-                ))
-                .build();
+        try {
+            InitiateAuthRequest authRequest = InitiateAuthRequest.builder()
+                    .authFlow(AuthFlowType.USER_PASSWORD_AUTH)
+                    .clientId(clientId)
+                    .authParameters(Map.of(
+                            "USERNAME", (String) request.get("email"),
+                            "PASSWORD", (String) request.get("password")
+                    ))
+                    .build();
 
-        InitiateAuthResponse response = cognitoClient.initiateAuth(authRequest);
-        return Map.of("statusCode", 200, "accessToken", response.authenticationResult().idToken());
+            InitiateAuthResponse response = cognitoClient.initiateAuth(authRequest);
+            return Map.of("statusCode", 200, "accessToken", response.authenticationResult().idToken());
+
+        } catch (CognitoIdentityProviderException e) {
+            return Map.of("statusCode", 500, "error", "Cognito signin error: " + e.awsErrorDetails().errorMessage());
+        }
     }
 }
